292123ecbb3813b58031cdc2826d8674ed86d661
Tevin Rivera <tevinrivera94@gmail.com>
2017-04-19 11:28:28 -0400

    Script now includes basic sketching functionality

--- script.js
+++ script.js
@@ -2,9 +2,8 @@
  * Created by tevdevelops on 4/17/17.
  */
 var defaultGrid = 16;
-
+var color = '#000000'; /* Default color is black*/
 function drawGrid(num) {
-    var counter = 0;
     $('.container').css('width', num * 20);
     var loops = num * num;
     for (i = 0; i < loops; i++) {
@@ -14,12 +13,17 @@ function drawGrid(num) {
         else {
             $("<div class='unit'></div>").appendTo('.container');
         }
-        counter ++
     }
-    console.log(num);
-    console.log(loops);
-    console.log(counter);
 }
+function draw() {
+    $(this).mouseenter(function(){
+        $(this).css('background-color', color);
+    });
+}
+
 $(document).ready(function () {
    drawGrid(defaultGrid);
+    $('.unit').mouseenter(function(){
+        $(this).css('background-color', color);
+    });
 });
